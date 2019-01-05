55ce51ff947035db73426abf7444b919fc2dff94
VinceFrancois <vincefrancois60@gmail.com>
2019-01-05 15:51:22 +0100

    oui

--- app/src/main/java/utt/if26/recordo_if26/MainActivity.java
+++ app/src/main/java/utt/if26/recordo_if26/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Recordo : Sonomètre");
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
